dd52a13b1d0b5fb8d629ef26c657e0af7d250c99
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-09-12 22:33:48 +0300

    Готово 2-е

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+ingredients.forEach((ingredient) => {
+  const element = document.createElement("li");
+  element.classList.add("item");
+  element.textContent = ingredient;
+  console.log(element);
+
+  const list = document.querySelector("#ingredients");
+
+  list.appendChild(element);
+});
