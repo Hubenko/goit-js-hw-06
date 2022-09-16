de0ad829732623394d53b978ace36368afa91e3e
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-09-16 14:47:20 +0300

    Готово 5-е

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const formInput = document.querySelector("#name-input");
+const formOutput = document.querySelector("#name-output");
+
+formInput.addEventListener("input", onFormSubmit);
+
+function onFormSubmit(event) {
+  //   formOutput.innerHTML = "";
+  formOutput.textContent = event.currentTarget.value;
+  if (formOutput.textContent === "") {
+    formOutput.textContent = "Anonymous";
+  }
+}
