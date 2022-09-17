69d29b623c6f2a7c43a6d6e77101de52ecd5c467
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-09-17 16:02:16 +0300

    Готово 9-е

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnColor = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+const body = document.querySelector("body");
+
+btnColor.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const currentColor = getRandomHexColor();
+  body.style.backgroundColor = currentColor;
+  textColor.textContent = currentColor;
+}
