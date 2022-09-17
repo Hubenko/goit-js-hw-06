cf00377a8656b3d2cafd5a61350dd8c060c1816a
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-09-17 15:33:59 +0300

    Готово 8-е

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  const formData = { email, password };
+
+  if (formData.email === "" || formData.password === "") {
+    alert("Введите значение");
+  } else {
+    console.log(formData);
+  }
+  form.reset();
+}
