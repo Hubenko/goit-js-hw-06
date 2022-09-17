b0533a6a72849b3c14d02407228470bc96ae8d5b
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-09-17 16:39:35 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,9 @@ const valid = document.querySelector("#validation-input");
 console.log(valid);
 const checkLength = valid.dataset.length;
 console.log(typeof "checkLength");
-valid.addEventListener("blur", onCheckСonditionInput);
+valid.addEventListener("blur", onCheckСondition);
 
-function onCheckСonditionInput(event) {
+function onCheckСondition(event) {
   if (event.currentTarget.value.length === Number(checkLength)) {
     valid.classList.add("valid");
   } else {
